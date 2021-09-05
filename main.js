4bbd1ea9cfdc975f739302b6a2c071a8f267a901
R <r@RdeMacBook-Air.local>
2021-09-05 21:16:11 +0800

    update

---
+++ main.js
@@ -0,0 +1,22 @@
+alert("click the button to change background color")
+console.log("#"+Math.random().toString(16).slice(3, 9))
+console.log("#"+Math.random().toString(16))
+console.log("#"+Math.random())
+console.log(Math.floor(Math.random()*2))
+console.log(Math.floor(Math.random()*3))
+console.log(Math.floor(Math.random()*4))
+console.log(Math.floor(Math.random()*5))
+
+
+function randonColor(){
+    return "#"+Math.random().toString(16).slice(3, 9);
+}
+console.log(randonColor())
+
+
+const img = document.querySelector("img");
+img.addEventListener("click",function(){
+    var body = document.getElementsByTagName("body")[0];
+    // body.style.backgroundColor = "red";
+    body.style.backgroundColor = randonColor();
+})
