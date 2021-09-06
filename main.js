7b5089c802b1454834d5cc80f66fad0528ab401b
R <r@RdeMacBook-Air.local>
2021-09-06 21:19:20 +0800

    update

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// Math.random(): print random number that between 0 to 1, 
+// toString(16): print exchange to Hexadecimal
+// slice(3,9): print 4th to 10th string
+
 console.log("#"+Math.random().toString(16).slice(3, 9))
 console.log("#"+Math.random().toString(16))
 console.log("#"+Math.random())
@@ -12,10 +16,14 @@ function randonColor(){
 }
 console.log(randonColor())
 
-
+// get all img element
 const img = document.querySelector("img");
+// whenever img is clicked, do something
 img.addEventListener("click",function(){
+    // get first body element , and call it body
     var body = document.getElementsByTagName("body")[0];
     // body.style.backgroundColor = "red";
+
+    // let body style that is backgroundColor become randonColor();
     body.style.backgroundColor = randonColor();
 })
